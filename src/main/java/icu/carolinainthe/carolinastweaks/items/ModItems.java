cee3e23ef143a98d575873606e9e09d4939b29bf
Carolina <carolinamitchell@outlook.co.nz>
2025-01-22 14:16:16 +1300

    Add licence notices to all programmatical components of the software

--- src/main/java/icu/carolinainthe/carolinastweaks/items/ModItems.java
+++ src/main/java/icu/carolinainthe/carolinastweaks/items/ModItems.java
@@ -1,3 +1,24 @@
+/*
+    Carolina's Tweaks - A Minecraft Fabric Mod with a collection of minor adjustments and tweaks to improve your game.
+    Copyright (C) 2025 Carolina Mitchell
+    
+    This licence notice only applies to non-asset components relating to this software. For the assets licence,
+    see the ASSETS_LICENCE.md file.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 //package icu.carolinainthe.carolinastweaks.items;
 //
 //import icu.carolinainthe.carolinastweaks.CarolinasTweaks;
